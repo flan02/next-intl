db9c8f2dc090e971ab4139c22382670f5a950d08
flan02 <chanivetdan@hotmail.com>
2024-06-18 23:51:42 -0300

    Some changes to responsive desing.

--- src/components/CurriculumVitae.tsx
+++ src/components/CurriculumVitae.tsx
@@ -51,11 +51,11 @@ const CurriculumVitae = ({ person }: Props) => {
   // {t('')}
   return (
     <main className='lg:w-[1000px] md:w-[768px] sm:w-[640px] min-w-[470px] mx-auto overflow-x-hidden'>
-      <article className='h-[94vh]  grid grid-cols-12 border border-gray-400'>
+      <article className='h-max-h-full md:h-[94vh] grid grid-cols-12 border border-gray-400'>
 
         <Aside open={open} />
 
-        <section className="col-span-12 md:col-span-8 bg-blue-300">
+        <section className="col-span-12 md:col-span-8 bg-blue-300 overflow-y-auto">
           <div className='flex justify-between mt-2'>
             <div className='ml-2 mt-2 space-y-2'>
               <h1 className='font-bold text-4xl pl-2 underline'>{t('name', { name: person.firstName + " " + person.lastName })}</h1>
@@ -66,7 +66,7 @@ const CurriculumVitae = ({ person }: Props) => {
             <Image src={yo} alt='Dan Chanivet' width={100} height={100} className='border border-transparent hover:border hover:border-black mr-2 rounded-full w-auto h-auto' />
           </div>
           <br />
-          <div className='space-y-2'>
+          <div className='space-y-4 md:space-y-2'>
             <h2 className='font-bold text-2xl pl-4 '>{t('profile')}</h2>
             <p className='pl-4 pr-4 text-sm'>{t('profile1')}</p>
             <p className='pl-4 pr-4 text-sm'>{t('profile2')}</p>
@@ -87,7 +87,7 @@ const CurriculumVitae = ({ person }: Props) => {
             <p className="pl-4 pr-4 text-sm">{t('employment2')}</p>
             <p className="pl-4 pr-4 text-sm">{t('employment3')}</p>
             <p className="ml-4 pr-4 text-sm"> <a href='https://www.2kceltics.xyz' target="_blank" className='-pl-4 text-md hover:underline'>www.2kceltics.xyz</a>. {t('employment4')}</p>
-            <p className="pl-4 pr-4 text-sm">{t('employment5')}</p>
+            <p className="pl-4 pr-4 pb-4 md:pb-0 text-sm">{t('employment5')}</p>
           </div>
         </section>
       </article>
